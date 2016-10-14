f32b4d8aee76f786be5ae4a23e42cf6d6b912977
Thomas Kern <thomas.kern1@gmx.at>
2016-10-14 14:12:23 +0200

    Laschi ist wirklich doof!

--- uart/uart0.c
+++ uart/uart0.c
@@ -10,10 +10,7 @@
 #include <avr/interrupt.h>
 #define BUFFER_SIZE_SEND 30
 #include <string.h>
-
-//choose correct General-File
 #include "General_644P.h"
-//#include "General_ATMega2560.h"
 
 volatile static char sendBuffer[BUFFER_SIZE_SEND];
 volatile static uint8_t sendIndex;
