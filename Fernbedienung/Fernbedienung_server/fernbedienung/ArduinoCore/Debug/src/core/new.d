c6c45c516c2e0eb398d1a6642f09e118f5412041
florianL21 <flo.laschober@gmail.com>
2016-10-14 14:43:56 +0200

    General cleanup

--- Fernbedienung/Fernbedienung_server/fernbedienung/ArduinoCore/Debug/src/core/new.d
+++
@@ -1,7 +0,0 @@
-src/core/new.d src/core/new.o: ../src/core/new.cpp \
- d:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
- d:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h
-
-d:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
-
-d:\programme\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stddef.h:
