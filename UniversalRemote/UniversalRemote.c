58970d999b67a65ef71458c5a960686d9aa78e78
florianL21 <flo.laschober@gmail.com>
2016-11-02 16:38:29 +0100

    Management of PID Controller over remote control
    added support for controller transmission over the processing script

--- UniversalRemote/UniversalRemote.c
+++ UniversalRemote/UniversalRemote.c
@@ -9,7 +9,7 @@
 #include "UniversalRemote.h"
 Joysticks RemoteControl;
 
-int Connected=true;
+Entry Entrys[NUM_MAX_ENTRYS]={};
 
 void rec(char c)
 {
@@ -17,16 +17,20 @@ void rec(char c)
 	static int index=0;
 	static int angle1=0,extend1=0;
 	Buffer[index++]=c;
-	if(c==';')
+	#ifdef DEBUG_UNIVERSALREMOTE
+		//uart0_putc(c);
+	#endif
+	if(Buffer[0]>='0'&&Buffer[0]<='9'&& c=='\n'&&Buffer[index-2]==';')
 	{
-		if(index<=2 && Connected==false)
+		#ifdef DEBUG_UNIVERSALREMOTE
+			//uart0_puts(Buffer);
+		#endif
+		if(index>=4)
 		{
-			Connected=true;
-			uart1_puts_int("A");
-			index=0;
-		}else if(Connected==true && index>=4)
-		{
-			index--;
+			#ifdef DEBUG_UNIVERSALREMOTE
+				//uart0_puts("Rec. remote");
+			#endif
+			index-=2;
 			
 			if(Buffer[1]==',')
 			{
@@ -47,7 +51,6 @@ void rec(char c)
 				#endif
 				angle1=0;
 			}
-			
 			
 			
 			if(Buffer[index-3]==',')
@@ -71,34 +74,66 @@ void rec(char c)
 			if(Buffer[index-1]=='L')
 			{
 				RemoteControl.L.angle=angle1;
-				RemoteControl.L.x=angle1;
+				RemoteControl.L.x=angle1-100;
 				RemoteControl.L.extend=extend1;
-				RemoteControl.L.y=extend1;
+				RemoteControl.L.y=extend1-100;
 			}else if(Buffer[index-1]=='R')
 			{
 				RemoteControl.R.angle=angle1;
-				RemoteControl.R.x=angle1;
+				RemoteControl.R.x=angle1-100;
 				RemoteControl.R.extend=extend1;
-				RemoteControl.R.y=extend1;
+				RemoteControl.R.y=extend1-100;
 			}
 			angle1=0;
 			index=0;
 		}else
 		{
-/*
-			if(Connected==true)
-				uart0_puts("t");
-			else if(Connected==false)
-				uart0_puts("f");*/
 			index=0;
 		}
+	}else if(Buffer[0]=='c'&& c=='\n'&&Buffer[index-2]==';')
+	{
+		#ifdef DEBUG_UNIVERSALREMOTE
+			//uart0_puts("Rec. command");
+		#endif
+		uint8_t index1=0;
+		//uart0_puts(Buffer);
+		if(Buffer[2]==',')
+		{
+			index1=Buffer[1]-'0';
+			/*uart0_putInt(index1);
+			uart0_putc(',');*/
+			if(Entrys[index1].setting!=NULL)
+			{
+				*Entrys[index1].setting=(Buffer[3]-'0')*100+(Buffer[4]-'0')*10+(Buffer[5]-'0');
+				/*uart0_putInt(*Entrys[index1].setting);
+				uart0_puts("\n\r");*/
+			}
+		}else if(Buffer[1]>='0'&&Buffer[1]<='9'&&Buffer[2]>='0'&&Buffer[2]<='9')
+		{
+			index1=(Buffer[1]-'0')*10+(Buffer[2]-'0');
+			/*uart0_putInt(index1);
+			uart0_putc(',');*/
+			if(Entrys[index1].setting!=NULL)
+			{
+				*Entrys[index1].setting=(Buffer[4]-'0')*100+(Buffer[5]-'0')*10+(Buffer[6]-'0');
+				/*uart0_putInt(*Entrys[index1].setting);
+				uart0_puts("\n\r");*/
+			}
+		}
+		//uart0_putInt(index1);
+		index=0;
 	}
 }
 
 void UniversalRemote_Init()
 {
 	uart1_registerCallBack(rec);
-	uart1_init_x(57600,1,1,1,1);
+	uart1_init_x(9600,1,1,1,1);
+	for(uint8_t i=0;i<NUM_MAX_ENTRYS;i++)
+	{
+		Entrys[i].setting=NULL;
+	}
+	//uart1_puts_int("dofile(\"client.lua\");");
 }
 
 Joysticks UniversalRemote_GetValues()
@@ -106,13 +141,11 @@ Joysticks UniversalRemote_GetValues()
 	return RemoteControl;
 }
 
-void UniversalRemote_waitForConnection()
+uint8_t UniversalRemote_addMenuEntry(uint16_t* pValue,char Label[],uint8_t type)
 {
-	while(Connected==false);
-	{
-		if(Connected==true)
-			uart0_puts("T");
-		else if(Connected==false)
-			uart0_puts("F");
-	}
+	//uart1_puts_int("");		//send command to add menu entry on Remote
+	static uint8_t index2=0;
+	Entrys[index2].setting=pValue;
+	index2++;
+	return index2-1;
 }
--- UniversalRemote/UniversalRemote.h
+++ UniversalRemote/UniversalRemote.h
@@ -9,19 +9,23 @@
 #ifndef UNIVERSALREMOTE_H_
 #define UNIVERSALREMOTE_H_
 
+#define NUM_MAX_ENTRYS		10
+#define NULL 0
+
 //choose correct General-File
-#include "General_644P.h"
-//#include "General_ATMega2560.h"
+//#include "General_644P.h"
+#include "General_ATMega2560.h"
 
 #include "uart1.h"
 #include "uart0.h"
 #include <stdbool.h>
 
+
 typedef struct {
-	uint16_t extend;
-	uint16_t angle;
-	int x;
-	int y;
+	int16_t extend;
+	int16_t angle;
+	int16_t x;
+	int16_t y;
 }coordinate;
 
 typedef struct {
@@ -31,15 +35,15 @@ typedef struct {
 
 typedef struct {
 	
-	int* setting;
-	int index;
-}Enty;
+	uint16_t* setting;
+	uint8_t index;
+}Entry;
 
 
 void UniversalRemote_Init();
 Joysticks UniversalRemote_GetValues();
 void UniversalRemote_waitForConnection();
-
+uint8_t UniversalRemote_addMenuEntry(uint16_t* pValue, char Label[], uint8_t type);
 
 
 
