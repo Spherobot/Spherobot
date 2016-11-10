1080f01eb5a3f223566116fcf18313fc304be787
kernthomas <thomas.kern1@gmx.at>
2016-11-10 09:27:35 +0100

    Initial commit
    Initial commit of the EEPROM. It is used to write and read the EEPROM

---
+++ EEPROM/eeprom.c
@@ -0,0 +1,46 @@
+/*
+ * eeprom.c
+ *
+ * Created: 18.02.2016 14:27:24
+ *  Author: Admin
+ */ 
+
+#include <avr/io.h>
+
+void EEPROPM_init()
+{
+	//Erase and Write in one operation
+	EECR &= ~((1<<EEPM1) | (1<<EEPM0));
+}
+
+void EEPROM_write(uint16_t uiAddress, uint8_t ucData)
+{
+	//Wait for completion of previous write 
+	while(EECR & (1<<EEPE));
+	
+	//Set up address and Data Registers
+	EEAR = uiAddress;
+	EEDR = ucData;
+	
+	//Write logical one to EEMPE 
+	EECR |= (1<<EEMPE);
+	
+	//Start eeprom write by setting EEPE
+	EECR |= (1<<EEPE);
+}
+
+uint8_t EEPROM_read(unsigned int uiAddress)
+{
+	//Wait for completion of previous write
+	while(EECR & (1<<EEPE));
+	
+	//Set up address register
+	EEAR = uiAddress;
+	
+	//Start eeprom read by writing EERE 
+	
+	EECR |= (1<<EERE);
+	
+	//Return data from Data Register
+	return EEDR;
+}
